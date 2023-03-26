7d28bc182966655329da12916ac5bdfdfc59b83d
unknown <goluchoudhary7808@gmail.com>
2023-03-26 22:59:01 +0530

    categoryadded

--- src/actions/category.action.js
+++ src/actions/category.action.js
@@ -1,8 +1,22 @@
 import axios from "axios"
 import axiosIntance from "../helpers/axios"
+import { categoryConstansts } from "./constants"
 export const getAllCategory=()=>{
     return async dispatch=>{
+        dispatch({type:categoryConstansts.GET_ALL_CATEGORIES_REQUEST})
         const res=await axiosIntance.get('/category/getcategory')
         console.log(res)
+        const {categoryList}=res.data
+        if(res.status===200){
+           dispatch({
+            type:categoryConstansts.GET_ALL_CATEGORIES_SUCCESS,
+            payload:{categories:categoryList}
+           })
+        }else{
+           dispatch({
+            type:categoryConstansts.GET_ALL_CATEGORIES_FAILURE,
+            payload:{error:res.data.error}
+           })
+        }
     }
 }
--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -2,20 +2,34 @@ import React, { useEffect } from 'react'
 import Layout from '../../components/Layout'
 import Sidebar from '../../components/Layout/sidebar';
 import { Container,Row,Col } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllCategory } from '../../actions';
 
-function Category() {
+const Category=() =>{
     const dispatch=useDispatch();
+    const category=useSelector(state=>state.category)
     useEffect(()=>{
+      console.log("Kk")
        dispatch(getAllCategory())
     },[])
+    const renderCategories=(categories)=>{
+               let myCategories=[];
+               for(let category of categories){
+                myCategories.push(
+                  <li>
+                    {category.name}
+                  </li>
+                )
+               }
+               return myCategories
+    }
   return (
    <>
      <Layout/>
      <Sidebar name="category"/>
-      <Container>
-        <Row>
+     <Container style={{marginLeft:'250px' ,width:'auto'}}>
+     
+        <Row >
             <Col md={12}>
               <div style={{display:'flex',justifyContent:'space-between'}}>
                 <h3>Category</h3>
@@ -23,7 +37,16 @@ function Category() {
               </div>
             </Col>
         </Row>
-      </Container>
+        <Row>
+          <Col md={2}>
+             <ul>
+              {renderCategories(category.categories)}
+             </ul>
+          </Col>
+        </Row>
+        </Container>
+     
+      
    </>
   )
 }
